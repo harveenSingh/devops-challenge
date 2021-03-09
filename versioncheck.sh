c599fe6fb4c52381038616fc6969f54d4d2181d1
harveenSingh <harveen.s@hotmail.com>
2021-03-09 15:01:24 -0500

    Changed the ip of EC2

--- versioncheck.sh
+++ versioncheck.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-if curl ecomp.harveensingh.com/version/version.html | grep "1.0.6" > /dev/null; then
-if curl --head ecomp.harveensingh.com | grep "200 OK" > /dev/null; then
+if curl ec2-3-23-102-151.us-east-2.compute.amazonaws.com/version/version.html | grep "1.0.6" > /dev/null; then
+if curl --head ec2-3-23-102-151.us-east-2.compute.amazonaws.com | grep "200 OK" > /dev/null; then
    echo "website version 1.0.6 is UP and RUNNING "
 fi
 else
